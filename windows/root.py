5f3d24c73826077491c637497bebffb40a452098
Filippo Lizzi <filippolizzipiggy@gmail.com>
2023-02-17 21:36:48 +0100

    Added newNoteForm in windows/root.py

--- windows/root.py
+++ windows/root.py
@@ -12,7 +12,15 @@ windowHeight = 600
 
 root = ctk.CTk()
 root.title("Crypted Note")
-root.resizable(False, False)
 root.geometry(f"{windowWidth}x{windowHeight}+{int(monitorWidth/2-windowWidth/2)}+{int(monitorHeight/2-windowHeight/2)}")
 
 rootFrame = ctk.CTkFrame(root)
+
+newNoteFormFrame = ctk.CTkFrame(rootFrame, fg_color="transparent")
+newNoteFormFrame.pack(pady=20 ,side=ctk.TOP)
+
+titleEntry = ctk.CTkEntry(newNoteFormFrame, width=500, height=40, font=("Helvetica", 20))
+titleEntry.grid(row=0, column=0)
+
+newNoteButton = ctk.CTkButton(newNoteFormFrame, text="New", width=70, height=40, font=("Helvetica", 20))
+newNoteButton.grid(row=0, column=1, padx=10)
