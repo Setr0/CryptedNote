15b610910dbd67fdc4280ef7bc42589fe2d606f2
Filippo Lizzi <filippolizzipiggy@gmail.com>
2023-02-20 22:06:54 +0100

    At the start of the app it checks if exists the json/notes.json file

--- Main.py
+++ Main.py
@@ -2,14 +2,19 @@ from windows.home import homeFrame, notesFrame
 from windows.root import root
 import customtkinter as ctk
 import json
+from os.path import exists
 from func.note import openNote
 from func.crypt import decrypt
-from func.file import read
+import func.file as file
 
 if __name__ == "__main__":
+
+    if not exists("json/notes.json"):
+        file.write(json.dumps({}, indent=4))
+
     homeFrame.pack(fill=ctk.BOTH, expand=True)
     
-    notesObject = json.loads(read())
+    notesObject = json.loads(file.read())
     notes = list(notesObject.keys())
 
     notes.sort()
